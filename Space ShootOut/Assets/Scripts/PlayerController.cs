d167df117b548b4414ec5b58f7e9ae62b1c1d06a
Tom Sauer <tom@exluto.com>
2017-11-02 20:31:59 -0400

    Adding player movement

--- Space ShootOut/Assets/Scripts/PlayerController.cs
+++ Space ShootOut/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float x_Velocity = 20.0f;
+	public Rigidbody x_rb;
+	public KeyCode x_BowVelocity;
+	public KeyCode  x_SternVelocity;
+
+	public KeyCode x_PortVelocity;
+	public KeyCode x_StarboordVelocity;
+	
+
 	// Use this for initialization
 	void Start () {
+		x_rb = GetComponent<Rigidbody>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetKeyDown(x_BowVelocity)) {
+		
+
+		}
+		if(Input.GetKeyDown(x_SternVelocity)) {
+		
+
+		}
+		if(Input.GetKeyDown(x_StarboordVelocity)) {
+			
+
+		}
+		if(Input.GetKeyDown(x_PortVelocity)) {
+			
+
+		}
 		
 	}
 }
