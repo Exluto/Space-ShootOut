01c0bbd5a85a3bb56cb7478dfa4df4f6d395e83c
Tom Sauer <tom@exluto.com>
2017-11-06 21:02:32 -0500

    adding drag

--- Space ShootOut/Assets/Scripts/Player/PlayerController.cs
+++ Space ShootOut/Assets/Scripts/Player/PlayerController.cs
@@ -2,19 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 	private float x_thrust = -20.0f;
 	private float x_ReThrust = 15.0f;
-	private float x_RiThrust = 5.0f;
-	private float x_LeThrust = -5.0f;
-	public float x_tilt;
+	private float x_RiThrust = 10.0f;
+	private float x_LeThrust = -10.0f;
+	private float x_tilt = 1;
 
 	private KeyCode x_ForwardVelocity;
-
 	private KeyCode x_ReverseVelocity;
 	private KeyCode x_RightVelocity;
 	private KeyCode x_LeftVelocity;
-
 	public Rigidbody x_rb;
 
 	void Start() {
@@ -26,40 +25,31 @@ public class PlayerController : MonoBehaviour {
 	}
 	void Update() {
 		if(Input.GetKey(x_ForwardVelocity)) {
-			//transform.position += Vector3.forward * Time.deltaTime * x_thrust;
 			x_rb.AddForce(0, 0, x_thrust, ForceMode.Acceleration);
 		}
-		//add Input.GetKeyUp to remove velocity
 
 		if(Input.GetKey(x_ReverseVelocity)) {
-		//	transform.position -= Vector3.forward * Time.deltaTime * x_RThrust;
 			x_rb.AddForce(0, 0, x_ReThrust, ForceMode.Acceleration);
-		
 		}
 
 		if(Input.GetKey(x_RightVelocity)) {
 			x_rb.AddForce(x_LeThrust, 0, 0, ForceMode.Acceleration);
 		}
+
 		if(Input.GetKey(x_LeftVelocity)) {
 			x_rb.AddForce(x_RiThrust, 0, 0, ForceMode.Acceleration);
-
 		}
-		// float moveHorizontal =  Input.GetAxis("Horizontal");
+
+		x_rb.rotation = Quaternion.Euler (0.0f, 0.0f, x_rb.velocity.x * -x_tilt);
+		x_rb.drag = 0.8f;
+	}
+}
+
+//transform.position += Vector3.forward * Time.deltaTime * x_thrust;
+//	transform.position -= Vector3.forward * Time.deltaTime * x_RThrust;
+
+// float moveHorizontal =  Input.GetAxis("Horizontal");
 		// float moveVertical = Input.GetAxis("Vertical");
 
 		// Vector3 Movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		// x_rb.velocity = Movement * x_speed;
-
-		x_rb.rotation = Quaternion.Euler (0.0f, 0.0f, x_rb.velocity.x * -x_tilt);
-	}
-
-
-
-
-
-
-
-
-
-
-}
