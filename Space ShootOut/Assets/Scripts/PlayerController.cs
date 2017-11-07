afc7fd37a887a903da31e64b1a022df6f0a2ea4d
Tom Sauer <tom@exluto.com>
2017-11-06 20:15:38 -0500

    Finally worked out a perfect playermovement script for what we need

--- Space ShootOut/Assets/Scripts/PlayerController.cs
+++ Space ShootOut/Assets/Scripts/PlayerController.cs
@@ -3,40 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	private float x_thrust = -20.0f;
+	private float x_ReThrust = 15.0f;
+	private float x_RiThrust = 5.0f;
+	private float x_LeThrust = -5.0f;
+	public float x_tilt;
 
-	public float x_Velocity = 20.0f;
+	private KeyCode x_ForwardVelocity;
+
+	private KeyCode x_ReverseVelocity;
+	private KeyCode x_RightVelocity;
+	private KeyCode x_LeftVelocity;
+
 	public Rigidbody x_rb;
-	public KeyCode x_BowVelocity;
-	public KeyCode  x_SternVelocity;
 
-	public KeyCode x_PortVelocity;
-	public KeyCode x_StarboordVelocity;
-	
-
-	// Use this for initialization
-	void Start () {
+	void Start() {
 		x_rb = GetComponent<Rigidbody>();
-		
+		x_ForwardVelocity = KeyCode.W;
+		x_ReverseVelocity = KeyCode.S;
+		x_RightVelocity = KeyCode.D;
+		x_LeftVelocity = KeyCode.A;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(x_BowVelocity)) {
+	void Update() {
+		if(Input.GetKey(x_ForwardVelocity)) {
+			//transform.position += Vector3.forward * Time.deltaTime * x_thrust;
+			x_rb.AddForce(0, 0, x_thrust, ForceMode.Acceleration);
+		}
+		//add Input.GetKeyUp to remove velocity
+
+		if(Input.GetKey(x_ReverseVelocity)) {
+		//	transform.position -= Vector3.forward * Time.deltaTime * x_RThrust;
+			x_rb.AddForce(0, 0, x_ReThrust, ForceMode.Acceleration);
 		
+		}
+
+		if(Input.GetKey(x_RightVelocity)) {
+			x_rb.AddForce(x_LeThrust, 0, 0, ForceMode.Acceleration);
+		}
+		if(Input.GetKey(x_LeftVelocity)) {
+			x_rb.AddForce(x_RiThrust, 0, 0, ForceMode.Acceleration);
 
 		}
-		if(Input.GetKeyDown(x_SternVelocity)) {
-		
+		// float moveHorizontal =  Input.GetAxis("Horizontal");
+		// float moveVertical = Input.GetAxis("Vertical");
 
-		}
-		if(Input.GetKeyDown(x_StarboordVelocity)) {
-			
+		// Vector3 Movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		// x_rb.velocity = Movement * x_speed;
 
-		}
-		if(Input.GetKeyDown(x_PortVelocity)) {
-			
-
-		}
-		
+		x_rb.rotation = Quaternion.Euler (0.0f, 0.0f, x_rb.velocity.x * -x_tilt);
 	}
+
+
+
+
+
+
+
+
+
+
 }
