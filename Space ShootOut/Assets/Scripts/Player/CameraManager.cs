a261e4152e4c669fc464854f168464eb59299c24
Tom Sauer <tom@exluto.com>
2017-11-08 21:08:22 -0500

    Added Projectiles, Added Multiple cameras
    Multiple cams not working properly Unity is auto switching, projectiles not always going in the right direction

---
+++ Space ShootOut/Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+	public Camera x_PlayerCamera;
+	public Camera x_RocketCamera;
+	private KeyCode x_PlayerView;
+
+/*	public void ShowPlayerView() {
+		x_PlayerCamera.enabled = true;
+		x_RocketCamera.enabled = false;
+	}
+
+	public void ShowRocketView() {
+		x_PlayerCamera.enabled = false;
+		x_RocketCamera.enabled = false;
+	} */
+
+	// Use this for initialization
+	void Start () {
+		x_PlayerView = KeyCode.R;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(x_PlayerView)) {
+			x_PlayerCamera.enabled = false;
+			x_RocketCamera.enabled = true;
+		}
+		
+	}
+}
